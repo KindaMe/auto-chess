fe15a4740c6e3d35bcee6527f51fd72b7d89880b
Patryk Kalinowski <kalinowski1999@gmail.com>
2021-11-22 06:17:36 +0100

    Add pawn master/children and virtual ability function.

--- auto-chess/auto-chess.cpp
+++ auto-chess/auto-chess.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <vector>
+
+class pawn
+{
+protected:
+	std::string name = "DEFAULT NAME";
+	int damage = 0;
+public:
+	virtual void ability()
+	{
+		std::cout << name << " MASTER ABILITY.";
+	}
+};
+
+class pawn1 : public pawn
+{
+public:
+	pawn1()
+	{
+		name = "PAWN 1";
+		damage = 5;
+	}
+	void ability()
+	{
+		std::cout << name << " CHILD ABILITY.\n";
+		std::cout << damage << " CHILD DAMAGE.\n";
+	}
+};
+
+class pawn2 : public pawn
+{
+public:
+	pawn2()
+	{
+		name = "PAWN 2";
+		damage = 10;
+	}
+	void ability()
+	{
+		std::cout << name << " CHILD ABILITY.\n";
+		std::cout << damage << " CHILD DAMAGE.\n";
+	}
+};
 
 int main()
 {
-	std::cout << "Hello Auto chess!\n";
+	std::vector<pawn*> pawnsPtrs;
+
+	pawn* pawn1Ptr;
+	pawn1 p1;
+	pawn1Ptr = &p1;
+
+	pawnsPtrs.push_back(pawn1Ptr);
+
+	pawnsPtrs[0]->ability();
+
+	system("pause>0");
 }
